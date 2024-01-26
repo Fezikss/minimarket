d65655fb4469839d110a80b0225b3d44435442f1
golang_user <smutalova45@gmail.com>
2024-01-26 16:55:59 +0500

    added

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+
+	"main.go/api"
+	"main.go/config"
+	"main.go/storage/postgres"
+)
+
+func main() {
+	cfg := config.Load()
+
+	store, err := postgres.New(cfg)
+	if err != nil {
+		log.Fatalln("error while connecting to db err:", err.Error())
+		return
+	}
+	defer store.Close()
+
+	server := api.New(store)
+
+	if err = server.Run("localhost:8088"); err != nil {
+		panic(err)
+	}
+}
